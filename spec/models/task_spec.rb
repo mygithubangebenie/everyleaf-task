23a9318059082de7576e9c1dfad08869145c1cda
mygithubangebenie <angebenie23@gmail.com>
2019-10-02 13:22:14 +0000

    for correcting content in spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "Validation does not pass if the name is empty" do
-    task = Task.new(name: 'benie', content: 'Failure test')
+    task = Task.new(name: '', content: 'Failure test')
     expect(task).not_to be_valid
   end
 
   it "Validation pass if content is not empty" do
-    task = Task.new(name: 'benie', content: 'Failure test', status:'completed', priority:'high', startdate:'2017-3-12', enddate:'2019-3-12')
-    expect(task).to be_valid
-    # Describe the contents here
+    task = Task.new(name: 'benie', content: '')
+    expect(task).not_to be_valid
   end
 
-  it "validation passes If content is described in name and content" do
-    # Describe the contents here
+  it "validation does not pass If  name and content are empty" do
+    task = Task.new(name: '', content: 'Failure test')
+    expect(task).not_to be_valid
   end
 end
