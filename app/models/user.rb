386a807ab3ff921f420c00f099c5bf22f388de28
mygithubangebenie <angebenie23@gmail.com>
2019-10-28 09:42:35 +0000

    correction of Add unique constraints to the users email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     validates :name, :email, :password, presence: true
     has_many :tasks, dependent: :destroy
     has_secure_password
+    validates :email, uniqueness: true
+def email_uniqueness
+ if AlternateAddress.find_by_email(self.email)
+   self.errors.add(:email, "is already in use by another account")
+ end
+end
 end
