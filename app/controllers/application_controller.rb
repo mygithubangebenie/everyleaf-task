b8919e257133046ab77921e450f778b8e3c89bbb
mygithubangebenie <angebenie23@gmail.com>
2019-11-08 13:24:23 +0000

    everyleaf task correction of step 22 up to 24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class ApplicationController < ActionController::Base
     end
     def new
     end
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
+    def authorize
+      redirect_to '/login' unless current_user
+    end
     
     def create
       user = User.new(user_params)
@@ -26,12 +33,4 @@ class ApplicationController < ActionController::Base
     end
     protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
-
-  def authorize
-    redirect_to '/login' unless current_user
-  end
 end
